06d97ce9589b7544c839fa164c4bdcd68caacd9e
Jen-Chieh Shen <jcs090218@gmail.com>
2021-10-15 01:03:55 +0800

    Add core

---
+++ sublime-fuzzy.el
@@ -0,0 +1,38 @@
+;;; sublime-fuzzy.el --- Fuzzy matching algorithm based on Sublime Text's string search  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Shen, Jen-Chieh
+;; Created date 2021-10-15 01:03:01
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Description: Fuzzy matching algorithm based on Sublime Text's string search.
+;; Keyword: sublime fuzzy algorithm
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; URL: https://github.com/jcs090218/sublime-fuzzy
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Fuzzy matching algorithm based on Sublime Text's string search.
+;;
+
+;;; Code:
+
+
+
+(provide 'sublime-fuzzy)
+;;; sublime-fuzzy.el ends here
